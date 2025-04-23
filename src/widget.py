9722ae4a78a88b0582a60918d7763419f6412d41
Alena Chaschevaia <alena.sarycheva.99@bk.ru>
2025-04-23 23:32:18 +0300

    add tests for widget.py

--- src/widget.py
+++ src/widget.py
@@ -1,3 +1,4 @@
+from typing import Any
 
 from src.masks import get_mask_account, get_mask_card_number
 
@@ -7,15 +8,15 @@ def mask_account_card(payment_method: str) -> Any:
     Для карт и счетов используется разные типы маскировки"""
     if payment_method.startswith("Счет"):
         return (
-            payment_method[:payment_method.find(" ")]
+            payment_method[: payment_method.find(" ")]
             + " "
-            + get_mask_account(payment_method[payment_method.find(" "):])
+            + get_mask_account(payment_method[payment_method.find(" ") :])
         )
     else:
         return payment_method[:-16] + " " + get_mask_card_number(payment_method[-16:])
 
 
-def gate_date(date: str) -> str:
+def get_date(date: str) -> str:
     """Функция, которая принимает на вход строку с датой в формате
     "2024-03-11T02:26:18.671407" и возвращает строку с датой в формате
     "ДД.ММ.ГГГГ"("11.03.2024")."""
